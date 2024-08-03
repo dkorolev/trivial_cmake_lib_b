fb53d4e1fef6231caac96128b69e81748b8c61ec
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-04 01:33:21 +0200

    Need to `PLS_IMPORT()` first.

--- src/trivial_cmake_lib_b.cc
+++ src/trivial_cmake_lib_b.cc
@@ -1,9 +1,9 @@
-#include "trivial_cmake_lib_b.h"
-#include "trivial_cmake_lib.h"
-
 #include "pls.h"
 
 PLS_IMPORT("trivial_cmake_lib", "https://github.com/github/dkorolev/trivial_cmake_lib");
+
+#include "trivial_cmake_lib_b.h"
+#include "trivial_cmake_lib.h"
 
 int trivial_cmake_lib_b() {
   return trivial_cmake_lib_add(100, 100);
